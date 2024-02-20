1967e79ec7c2fea2e2670b36b9259e7acb48d920
hammadbhatti20 <hammadaslam1052000@gamil.com>
2024-02-20 13:20:09 +0500

    JS Practice Test Done

---
+++ app.js
@@ -0,0 +1,131 @@
+//Chapter No: 1
+
+//Q1
+alert("Error! Please enter a valid password");
+
+//Q2
+document.write("Error! Please enter a valid password");
+
+//Q3
+document.write("Welcome to JS Land... \n Happy Coding!");
+
+//Q4
+alert("Welcome to JS Land...");
+alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+//Q5
+console.log("Hello... I can run JS through my web browser's console");
+
+
+//Chapter No: 2
+
+//Q1
+var username = "hammadbhatti20";
+
+//Q2
+var myName = "Hammad Bhatti";
+
+//Q3
+var message = "Hello World";
+alert(message);
+
+//Q4
+var studentName = "Jhone Doe", hisAge = "15 years old", qualification = "Certified Mobile Application Development";
+alert(studentName, hisAge, qualification);
+
+//Q5
+var brokenText = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(brokenText);
+
+//Q6
+var email = "hammadaslam1052000@gmail.com";
+alert(`My email address is ${email}`);
+
+//Q7
+var book = "A smarter way to learn JavaScript";
+alert(book);
+
+//Q8
+document.write("Yah! I can write HTML content through JavaScript");
+
+//Q9
+var str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+
+//Chapter No: 3
+
+//Q1
+var age = 24;
+alert(`I am ${age} year old`);
+
+//Q2
+var visitedWebPage = 14;
+document.write(`You have visited this site ${visitedWebPage} times`);
+
+//Q3
+var birthYear = 2000;
+document.write(`My birth year is ${birthYear} \n Data type of ddeclared variable is number`);
+
+//Q4
+var visitorName = "JohnDoe", productTitle = "T-shirt", quantity = 5;
+document.write(`${visitorName} ordered ${quantity} ${productTitle}(s) on XYZ Clothing store`);
+
+
+
+//Chapter 1 (Alerts)
+
+//Q1
+var firstName = "Hammad", lastName = "Bhatti", email = "hammadaslam1052000@gmail.com", phoneNumber = "03232084450", password = "abcdefgh";
+alert(`${firstName} \n ${lastName} \n ${email} \n ${phoneNumber} \n ${password}`);
+
+//Q2
+alert(`${firstName} ${lastName} learning JavaScript!`);
+
+//Q3
+alert("HELLO WORLD");
+
+
+//Chapter 2 (Variables for string)
+
+//Q1
+var userIdDetail = "Roose Bolton";
+
+//Q2
+var myFavoriteSports;
+myFavoriteSports = "Cricket";
+
+//Q3
+var teamName = "Austrailia";
+alert(teamName);
+
+//Q4
+var bestMan = "Charlie";
+var bestMan = "HammadBhatti";
+alert(bestMan);
+
+
+//Chapter 3 (Variables for numbers)
+
+//Q1
+var caseQty;
+
+//Q2
+caseQty = 144;
+
+//Q3
+var num = 9;
+
+//Q4
+var num2;
+num = 2;
+
+//Q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+//Q6
+var num3 = 20;
+var num4 = num3 + 10;
+console.log(num4);
